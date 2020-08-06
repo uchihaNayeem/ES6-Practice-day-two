aac435b946edf1cca42c665c7dbaca054a2d07a0
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-08-07 02:40:01 +0600

    truthy n falsy

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-console.log(`Day Two Started`);
+// 24.02
+
+//TRUTHY AND FALSY VALUES
+
+//All Falsy's
+let name ;
+let name1 = 0;
+let name2 = undefined;
+let name3= ``; // just empty string is also falsey
+let name4 = false;
+let name5 = NaN;
+let name6 = null;
+
+//All Thuthy
+
+let nam = ` `;
+let nam1 = `My Name`;
+let nam2 = true;
+
+
+if (name6) {
+  console.log(`this is true`);
+  
+}else{
+  console.log(`This is false`);
+}
+
+/* another example */
+
+let namay = 0;
+
+if (namay || namay == 0) {
+  console.log(`this is true`);
+  
+}else{
+  console.log(`This is false`);
+}
+
+
+
+/* --- END --- */
+
+//24.03
+
+
+
